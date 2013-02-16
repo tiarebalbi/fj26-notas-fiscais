1e712ccf1d7ebcd792c456662c523680a145e7e3
Tiare Balbi <tiarebalbi@me.com>
2013-02-16 16:48:20 -0200

    Update projeto

--- src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -15,6 +15,10 @@ public class ProdutoBean {
 	private Produto produto = new Produto();
 	
 	private List<Produto> produtos;
+
+	private double somatoria = 0;
+	
+	private String mensagem = "";
 	
 	public Produto getProduto() {
 		return produto;
@@ -29,18 +33,23 @@ public class ProdutoBean {
 		
 		if(this.produto.getId() == null) {
 			dao.adiciona(this.produto);
+			this.mensagem = "Registro Realizado com Sucesso";
 		}else {
+			this.mensagem = "Registro Atualizado com Sucesso";
 			dao.atualiza(produto);
 		}
 		this.produto = new Produto();
 		this.produtos = dao.listaTodos();
+		this.somatoria = this.doSomatoria(produtos);
 	}
 	
 	public List<Produto> getProdutos() {
 		if(this.produtos == null) {
 			DAO<Produto> dao = new DAO<Produto>(Produto.class);
 			this.produtos = dao.listaTodos();
+			this.somatoria = this.doSomatoria(produtos);
 		}
+		this.mensagem = "";
 		return this.produtos;
 	}
 	
@@ -48,10 +57,30 @@ public class ProdutoBean {
 		DAO<Produto> dao = new DAO<Produto>(Produto.class);
 		dao.remove(produto);
 		this.produtos = dao.listaTodos();
+		this.somatoria = this.doSomatoria(produtos);
+		this.mensagem = "Registro Removido com Sucesso";
 	}
 	
 	public void clean() {
 		this.produto = new Produto();
+	}
+	
+	public double getSomatoria () {
+		return this.somatoria;
+	}
+	
+	public String getMensagem () {
+		return this.mensagem;
+	}
+	
+	private double doSomatoria(List<Produto> produtos) {
+		double valor = 0;
+		
+		for (Produto produto : produtos) {
+			valor += produto.getPreco();
+		}
+		
+		return valor;
 	}
 	
 }
